3196c41a55dc3209c47df78d33afb61e3fbda72b
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2023-01-25 16:35:14 +0300

    Update penetration_testing3.py
    Add a feature to test for directory traversal vulnerabilities by sending payloads such as "../../etc/passwd" in the URL or form data and checking for sensitive information in the response.
    Add a feature to test for weak credentials by attempting to login with a list of commonly used passwords and username combinations.
    Add a feature to check for missing or weak HTTP headers, such as "X-XSS-Protection" and "Content-Security-Policy".
    Add a feature to check for clickjacking vulnerabilities by adding a "X-Frame-Options" header to the request and checking for the presence of the header in the response.
    Add a feature to check for open redirect vulnerabilities by sending payloads with redirection URLs to the application and check if the redirection occurs without any validation.
    Add a feature to check for insecure communication by checking if the website uses HTTPS and a valid SSL certificate.
    Add a feature to check for insecure session management by checking if the session ID is sent in the URL or as a cookie without proper encryption.
    Add a feature to check for broken authentication and session management by attempting to access the application with an expired or tampered session ID.

--- penetration_testing3.py
+++ penetration_testing3.py
@@ -38,3 +38,62 @@ files = {"file": ("test.txt", "hello world")}
 r = requests.post(website + "/upload", headers=headers, files=files)
 if "upload successful" in r.text.lower():
     print("Insecure file upload vulnerability found.")
+    
+# Test for directory traversal vulnerabilities
+payloads = ["../../etc/passwd", "..\\..\\Windows\\System32\\config\\SAM"]
+for payload in payloads:
+    r = requests.get(website + "/" + payload, headers=headers)
+    if "root:" in r.text or "Administrator:" in r.text:
+        print("Directory traversal vulnerability found with payload: " + payload)
+
+# Test for weak credentials
+common_usernames = ["admin", "root", "test"]
+common_passwords = ["password", "123456", "admin"]
+for username in common_usernames:
+    for password in common_passwords:
+        r = requests.post(website + "/login", headers=headers, data={"username": username, "password": password})
+        if "logged in" in r.text.lower():
+            print("Weak credentials found. username: " + username + " password  " + password)
+
+# Test for missing or weak HTTP headers
+headers_to_check = ["X-XSS-Protection", "Content-Security-Policy"]
+r = requests.get(website, headers=headers)
+for header in headers_to_check:
+    if header not in r.headers:
+        print("Missing " + header + " header.")
+    elif r.headers[header] == "0" or r.headers[header] == "":
+        print("Weak " + header + " header.")
+
+# Test for clickjacking vulnerabilities
+r = requests.get(website, headers=headers)
+if "X-Frame-Options" not in r.headers:
+    print("Clickjacking vulnerability found. No X-Frame-Options header found in the response.")
+
+# Test for open redirect vulnerabilities
+redirect_urls = ["http://evil.com", "https://attacker.com"]
+for redirect_url in redirect_urls:
+    r = requests.get(website + "?redirect=" + redirect_url, headers=headers)
+    if r.url == redirect_url:
+        print("Open redirect vulnerability found with URL: " + redirect_url)
+
+# Test for insecure communication
+if not website.startswith("https"):
+    print("Insecure communication. Website is not using HTTPS.")
+else:
+    try:
+        r = requests.get(website, headers=headers, verify=True)
+    except requests.exceptions.SSLError as e:
+        print("Insecure communication. Invalid SSL certificate: " + str(e))
+
+# Test for insecure session management
+r = requests.get(website, headers=headers)
+if "sessionid" in r.cookies:
+    session_id = r.cookies["sessionid"]
+    if session_id[-5:] == "12345":
+        print("Insecure session management. Session ID is not properly encrypted.")
+    r = requests.get(website + "/logout", headers=headers, cookies={"sessionid": session_id})
+    r = requests.get(website, headers=headers, cookies={"sessionid": session_id})
+    if "logged in" in r.text.lower():
+        print("Broken authentication and session management. Able to access the application with an expired or tampered session ID.")
+
+
